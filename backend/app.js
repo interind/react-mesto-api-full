25ea85ba2ba4bc09df742f68007b33ddd55f56b7
Aleksandr <interind@yandex.ru>
2021-01-17 12:52:28 +0300

    server

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const config = require('config');
 const { errors } = require('celebrate');
 const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger.js');
@@ -10,19 +9,9 @@ const routerUsers = require('./routes/users.js');
 const routerCards = require('./routes/cards.js');
 const routerError = require('./routes/error.js');
 
-const PORT = process.env.PORT || config.get('PORT');
-const BASE_PATH = `http://localhost:${PORT}`;
 const app = express();
 app.disable('x-powered-by');
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', `${BASE_PATH}`);
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  next();
-});
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
