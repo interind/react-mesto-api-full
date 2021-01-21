378d3c723beecdf9e9d2cf5b85c5fb7f68982f76
Aleksandr <interind@yandex.ru>
2021-01-21 06:53:11 +0300

    delete crash-test

--- backend/app.js
+++ backend/app.js
@@ -17,11 +17,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
 
 app.use(routerAuth);
 app.use(routerUsers);
